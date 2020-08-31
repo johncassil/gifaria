eb54df75ed997f165e7c97a598c692e002c8a8a8
John Paul Cassil <john@johncassil.com>
2020-08-30 21:15:29 -0400

    Added more UI/CSS tweaks and cleaned up comments!

--- app.R
+++ app.R
@@ -1,7 +1,3 @@
-#to do list
-# make the UI pretty
-# push to shinyapps.io
-
 library(shiny)
 library(DT)
 library(dplyr)
@@ -14,8 +10,7 @@ library(tibble)
 library(stringr)
 
 
-## On startup:
-
+## On Startup:
 #Get the books for the dropdown:
 
 #Make API Call:
@@ -31,131 +26,135 @@ extract_titles <- function(data){data %>% select(title)}
 tanakh_titles <- tanakh_only$contents %>% purrr::map_dfr(extract_titles)
 
 
-
-
-##########################################
-
-
-
-ui <- navbarPage(theme = "sandstone.min.css",
-    
-    title = "ג",
-    tabPanel("gipharia",
-            # icon  = icon("tasks"),
-             sidebarLayout(
-               
-               sidebarPanel(
-                 
-                 selectInput(inputId = "text_input",
-                             label = "Select a text:",
-                             choices = tanakh_titles$title),
-                 uiOutput("chapter_input_dropdown"),
-                 br(),
-                 HTML('<center><h3>Click on a verse to get GIFs!</h3></center>'),
-                 
-                 dataTableOutput(outputId = "read_chapter")
-               ),
-               
-               
-               mainPanel(
-                 
-                 
-                 
-                 uiOutput("gif_view")
-                 
-               ))
-    )
+##UI
+ui <- shinyUI(
+  tagList(tags$head(tags$style(type = 'text/css','.navbar-nav{display:none;}')),
+          
+          navbarPage(
+            
+            theme = "sandstone.min.css",
+            
+            title = "ג gipharia",
+            
+            tabPanel("",
+                     sidebarLayout(
+                       
+                       sidebarPanel(
+                         
+                         selectInput(inputId = "text_input",
+                                     label = "Select a text:",
+                                     choices = tanakh_titles$title),
+                         
+                         uiOutput("chapter_input_dropdown"),
+                         
+                         HTML('<center><h3>Click on a verse to get GIFs!</h3></center>'),
+                         
+                         dataTableOutput(outputId = "read_chapter")
+                         
+                       ),
+                       
+                       mainPanel(
+                         
+                         uiOutput("gif_view")
+                         
+                       )
+                       
+                     ),
+                     hr(),
+                     HTML('<center><h5>Powered by <a href="https://sefaria.org/">Sefaria</a> & <a href = "https://giphy.com/">Giphy</a>.  Created with 💜 by <a href= "http://johncassil.com/">John Cassil</a> & <a href = "https://github.com/CodingTigerTang/">Tiger Tang</a></h5></center>'),
+                     
+                     )
+                     
+          )
+  )
 )
+
+
+##Functions
 
 find_ele <- function(x, name) {
   x_names <- names(x)
   return(x[which(x_names == name)][[1]])
 }
+
+#API call to get chapter options:
 get_options <- function(input){
   res <- httr::GET(paste0('https://www.sefaria.org/api/shape/', input))
-  #Unpack it:
   data <- jsonlite::fromJSON(rawToChar(res$content))
   options <- 1:data$length %>% as.integer()
 }
 
-
-#Function to get the texts from Sefaria's API:
+#Function to get the text for chapters from Sefaria's API:
 grab_data <- function(text, chapter){
-  #Get the data:
   res <- httr::GET(paste0('https://www.sefaria.org/api/texts/',
                           text,".", chapter, 
                           '?context=0&pad=0'))
-  #Unpack it:
   data <- jsonlite::fromJSON(rawToChar(res$content)) %>% .$text
 }
 
+
+#Server
+
 server <- function(input, output, session) {
-    
-
-data <- reactive(grab_data(as.character(input$text_input), 
-                      as.character(input$chapter_input)) %>% 
-            unlist() %>% 
-            unlist() %>% 
-              tibble::as_tibble_col(column_name = "Text") %>%
-              mutate(verse = row_number(),
-                     Text = stringr::str_replace_all(Text, '<i>', ''),
-                     Text = stringr::str_replace_all(Text, '</i>', '')) %>% 
-              select(Verse = verse, Text)
-)
-   
-output$read_chapter <- renderDataTable({
-      req(data())
   
-      data() %>% 
-        DT::datatable(rownames = F,selection=list(mode="single", target="row"),
-                      options = list(dom="tipr"))
-   })
+  data <- reactive({
+    grab_data(as.character(input$text_input), 
+              as.character(input$chapter_input)) %>% 
+      unlist() %>% 
+      unlist() %>% 
+      tibble::as_tibble_col(column_name = "Text") %>%
+      mutate(verse = row_number(),
+             Text = stringr::str_replace_all(Text, '<i>', ''),
+             Text = stringr::str_replace_all(Text, '</i>', '')) %>% 
+      select(Verse = verse, Text)
+  })
+  
+  output$read_chapter <- renderDataTable({
+    req(data())
     
-    
-    
-    chapter_dropdown_values <- reactive({
-        input$text_input %>% get_options()
-    })
-    
-    
-    output$chapter_input_dropdown <- renderUI({
-        selectInput(inputId = "chapter_input",
-                    label = "Select a chapter: ",
-                    choices = chapter_dropdown_values()
-        )
-        })
-
-    
-    
-
-    
-prev_gifs <- reactive({
-       req(search_text())
-        out <- suppressWarnings(
-            gif_search(search_text(),
-                       img_format = c("fixed_height_small", "downsized",
-                                      "downsized_medium", "original")
-                      ))
-        if (is.null(out)) return(NULL)
-        return(out)
-    })
-        
-search_text <- reactive({
+    data() %>% 
+      DT::datatable(rownames = F,selection=list(mode="single", target="row"),
+                    options = list(dom="tipr"))
+  })
+  
+  chapter_dropdown_values <- reactive({
+    input$text_input %>% get_options()
+  })
+  
+  output$chapter_input_dropdown <- renderUI({
+    selectInput(inputId = "chapter_input",
+                label = "Select a chapter: ",
+                choices = chapter_dropdown_values()
+    )
+  })
+  
+  prev_gifs <- reactive({
+    req(search_text())
+    out <- suppressWarnings(
+      gif_search(search_text(),
+                 img_format = c("fixed_height_small", "downsized",
+                                "downsized_medium", "original")
+      ))
+    if (is.null(out)) return(NULL)
+    return(out)
+  })
+  
+  search_text <- reactive({
     req(data)
     req(input$read_chapter_rows_selected)
     data()[input$read_chapter_rows_selected,] %>% 
-        pull(Text)
-})
-
-output$gif_view <- renderUI({
-            req(prev_gifs())
-            apply(prev_gifs(), 1, function(x) {
-                actionLink(find_ele(x, "id"), title = find_ele(x, "slug"),
-                           label = NULL, class = "gifpreview", icon = NULL,
-                           tags$img(src = find_ele(x, "downsized_medium")))
-            })
-        })
-
+      pull(Text)
+  })
+  
+  output$gif_view <- renderUI({
+    req(prev_gifs())
+    apply(prev_gifs(), 1, function(x) {
+      actionLink(find_ele(x, "id"), title = find_ele(x, "slug"),
+                 label = NULL, class = "gifpreview", icon = NULL,
+                 tags$img(src = find_ele(x, "downsized_medium")))
+    })
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
